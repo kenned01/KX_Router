9f83c0015d86b670cbe74bcac82f3a2d93d92e45
kenned <nedmena@gmail.com>
2020-09-25 19:18:22 -0600

    demo

---
+++ Demo/README.md
@@ -0,0 +1,7 @@
+# KX_Router Demo
+
+To run this demo just clone the repository and 
+turn the php server on. Then point the requests to the 
+index.php file that's inside demo folder
+
+you can use postman to test all the request [postman](https://www.postman.com/downloads/)
---
+++ Demo/index.php
@@ -0,0 +1,29 @@
+<?php
+
+	require_once __DIR__."/../KX_Router.php";
+	$App = new KX_Router;
+
+	
+	$App->get(function ($res){
+		echo json_encode($res);
+	});
+
+	$App->post(function ($res){
+		echo json_encode($res);
+	});
+
+	$App->put(function ($res){
+		echo json_encode($res);
+	});
+
+	$App->delete(function ($res){
+		echo json_encode($res);
+	});
+
+	$App->run();
+	
+
+
+
+	
+
